1440dcdcd1123388043804c241aab552e0ac4a3c
chayeoi <chayeoikeem@gmail.com>
2019-06-15 16:29:19 +0900

    track mouse position

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+import useMousePosition from './useMousePoisition';
 
-function App() {
+const pitch = keyframes`
+  0% {
+    width: 100px;
+    height: 100px;
+    background-color: #ee5555;
+  }
+  100% {
+    width: 200px;
+    height: 200px;
+    background-color: #f3fbca;
+  }
+`
+
+const S = {
+  Wrapper: styled.div`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    border: 5px solid #ee5555;
+  `,
+  Cursor: styled.div.attrs(({ position }) => ({
+    style: {
+      top: `${position.y}px`,
+      left: `${position.x}px`,
+    }
+  }))`
+    position: absolute;
+    transform: translate(-50%, -50%);
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: gold;
+    animation: 1s ${pitch} ease-out infinite alternate;
+  `,
+};
+
+const App = () => {
+  const position = useMousePosition();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.Wrapper>
+      <div>
+        <S.Cursor position={position} />
+        <div>x: {position.x}</div>
+        <div>y: {position.y}</div>
+      </div>
+    </S.Wrapper>
   );
-}
+};
 
 export default App;
